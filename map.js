78fbae6db6a2f7dfaf4441370240b922f4f812c3
ImeshGu <imeshguna3@gmail.com>
2023-10-10 02:52:16 +0530

    initial

---
+++ map.js
@@ -0,0 +1,193 @@
+const gpsData = [
+    {lat:36.0914451283199,lng:114.97412109375},
+    {lat:36.1945366033266,lng:115.435546875},
+    {lat:36.1945366033266,lng:115.34326171875},
+    {lat:36.1945366033266,lng:115.06640625},
+    {lat:36.1945366033266,lng:114.97412109375},
+    {lat:36.1945366033266,lng:114.8818359375},
+    {lat:36.1945366033266,lng:114.78955078125},
+    {lat:36.1945366033266,lng:114.697265625},
+    {lat:36.1945366033266,lng:114.60498046875},
+    {lat:36.1945366033266,lng:114.5126953125},
+    {lat:36.2976280783333,lng:115.34326171875},
+    {lat:36.2976280783333,lng:115.2509765625},
+    {lat:36.2976280783333,lng:115.15869140625},
+    {lat:36.2976280783333,lng:115.06640625},
+    {lat:36.2976280783333,lng:114.97412109375},
+    {lat:36.2976280783333,lng:114.8818359375},
+    {lat:36.2976280783333,lng:114.78955078125},
+    {lat:36.2976280783333,lng:114.697265625},
+    {lat:36.2976280783333,lng:114.60498046875},
+    {lat:36.2976280783333,lng:114.5126953125},
+    {lat:36.2976280783333,lng:114.42041015625},
+    {lat:36.2976280783333,lng:114.328125},
+    {lat:36.2976280783333,lng:114.23583984375},
+    {lat:36.2976280783333,lng:114.1435546875},
+    {lat:36.4007195533399,lng:115.34326171875},
+    {lat:36.4007195533399,lng:115.2509765625},
+    {lat:36.4007195533399,lng:115.15869140625},
+    {lat:36.4007195533399,lng:115.06640625},
+    {lat:36.4007195533399,lng:114.97412109375},
+    {lat:36.4007195533399,lng:114.8818359375},
+    {lat:36.4007195533399,lng:114.78955078125},
+    {lat:36.4007195533399,lng:114.697265625},
+    {lat:36.4007195533399,lng:114.60498046875},
+    {lat:36.4007195533399,lng:114.42041015625},
+    {lat:36.4007195533399,lng:114.328125},
+    {lat:36.4007195533399,lng:114.23583984375},
+    {lat:36.4007195533399,lng:114.1435546875},
+    {lat:36.4007195533399,lng:114.05126953125},
+    {lat:36.4007195533399,lng:113.958984375},
+    {lat:36.4007195533399,lng:113.86669921875},
+    {lat:36.5038110283466,lng:115.2509765625},
+    {lat:36.5038110283466,lng:115.15869140625},
+    {lat:36.5038110283466,lng:115.06640625},
+    {lat:36.5038110283466,lng:114.97412109375},
+    {lat:36.5038110283466,lng:114.8818359375},
+    {lat:36.5038110283466,lng:114.78955078125},
+    {lat:36.5038110283466,lng:114.697265625},
+    {lat:36.5038110283466,lng:114.60498046875},
+    {lat:36.5038110283466,lng:114.5126953125},
+    {lat:36.5038110283466,lng:114.42041015625},
+    {lat:36.5038110283466,lng:114.328125},
+    {lat:36.5038110283466,lng:114.23583984375},
+    {lat:36.5038110283466,lng:114.1435546875},
+    {lat:36.5038110283466,lng:114.05126953125},
+    {lat:36.5038110283466,lng:113.958984375},
+    {lat:36.5038110283466,lng:113.86669921875},
+    {lat:36.6069025033533,lng:115.34326171875},
+    {lat:36.6069025033533,lng:115.2509765625},
+    {lat:36.6069025033533,lng:115.15869140625},
+    {lat:36.6069025033533,lng:115.06640625},
+    {lat:36.6069025033533,lng:114.97412109375},
+    {lat:36.6069025033533,lng:114.8818359375},
+    {lat:36.6069025033533,lng:114.78955078125},
+    {lat:36.6069025033533,lng:114.697265625},
+    {lat:36.6069025033533,lng:114.60498046875},
+    {lat:36.6069025033533,lng:114.5126953125},
+    {lat:36.6069025033533,lng:114.42041015625},
+    {lat:36.6069025033533,lng:114.328125},
+    {lat:36.6069025033533,lng:114.23583984375},
+    {lat:36.6069025033533,lng:114.1435546875},
+    {lat:36.6069025033533,lng:114.05126953125},
+    {lat:36.6069025033533,lng:113.958984375},
+    {lat:36.6069025033533,lng:113.86669921875},
+    {lat:36.7099939783599,lng:115.435546875},
+    {lat:36.7099939783599,lng:115.34326171875},
+    {lat:36.7099939783599,lng:115.2509765625},
+    {lat:36.7099939783599,lng:115.15869140625},
+    {lat:36.7099939783599,lng:115.06640625},
+    {lat:36.7099939783599,lng:114.97412109375},
+    {lat:36.7099939783599,lng:114.8818359375},
+    {lat:36.7099939783599,lng:114.78955078125},
+    {lat:36.7099939783599,lng:114.697265625},
+    {lat:36.7099939783599,lng:114.60498046875},
+    {lat:36.7099939783599,lng:114.5126953125},
+    {lat:36.7099939783599,lng:114.42041015625},
+    {lat:36.7099939783599,lng:114.328125},
+    {lat:36.7099939783599,lng:114.23583984375},
+    {lat:36.7099939783599,lng:114.1435546875},
+    {lat:36.7099939783599,lng:114.05126953125},
+    {lat:36.7099939783599,lng:113.958984375},
+    {lat:36.7099939783599,lng:113.86669921875},
+    {lat:36.8130854533666,lng:115.6201171875},
+    {lat:36.8130854533666,lng:115.52783203125},
+    {lat:36.8130854533666,lng:115.435546875},
+    {lat:36.8130854533666,lng:115.34326171875},
+    {lat:36.8130854533666,lng:115.2509765625},
+    {lat:36.8130854533666,lng:115.15869140625},
+    {lat:36.8130854533666,lng:115.06640625},
+    {lat:36.8130854533666,lng:114.97412109375},
+    
+    //Add more data points here...
+];
+
+// function initMap() {
+//     let mapOptions = {
+//        center: new google.maps.LatLng('7.8731','80.7718'),
+//        zoom: 7
+//     }
+//     let map = new google.maps.Map(document.getElementById('map'),mapOptions);
+
+    
+
+// }
+/////////////////////////////////
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15, // Set your desired zoom level
+        center: { lat: gpsData[0].lat, lng: gpsData[0].lng }, // Set the initial map center
+        mapId: '50b734b33f9a860d'
+    });
+
+    // Create an array to hold LatLng objects
+    const routeCoordinates = [];
+
+    // Process GPS data and convert it to LatLng objects
+    gpsData.forEach((point) => {
+        const latLng = new google.maps.LatLng(point.lat, point.lng);
+        routeCoordinates.push(latLng);
+    });
+
+    // Create a Polyline to display the route on the map
+    const routePath = new google.maps.Polyline({
+        path: routeCoordinates,
+        geodesic: true,
+        strokeColor: "#FF0000", // Line color
+        strokeOpacity: 1.0,
+        strokeWeight: 2, // Line width
+    });
+
+    // Set the Polyline on the map 
+    routePath.setMap(map);
+}
+
+
+
+// const route1Data = [
+//     { lat: 37.7749, lng: -122.4194, timestamp: "2023-09-25T10:00:00Z" },
+//     { lat: 37.7755, lng: -122.4183, timestamp: "2023-09-25T10:15:00Z" },
+//     { lat: 37.7762, lng: -122.4172, timestamp: "2023-09-25T10:30:00Z" },
+//     // Add more data points for route 1...
+// ];
+
+// const route2Data = [
+//     { lat: 37.7701, lng: -122.4224, timestamp: "2023-09-25T10:00:00Z" },
+//     { lat: 37.7720, lng: -122.4241, timestamp: "2023-09-25T10:15:00Z" },
+//     { lat: 37.7739, lng: -122.4260, timestamp: "2023-09-25T10:30:00Z" },
+//     // Add more data points for route 2...
+// ];
+
+// function initMap() {
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 15, // Set your desired zoom level
+//         center: { lat: gpsData[0].lat, lng: gpsData[0].lng }, // Set the initial map center
+//         mapId: '50b734b33f9a860d'
+//     });
+
+//     // Function to create and display a route
+//     function displayRoute(routeData, color) {
+//         const routeCoordinates = [];
+
+//         routeData.forEach((point) => {
+//             const latLng = new google.maps.LatLng(point.lat, point.lng);
+//             routeCoordinates.push(latLng);
+//         });
+
+//         const routePath = new google.maps.Polyline({
+//             path: routeCoordinates,
+//             geodesic: true,
+//             strokeColor: color, // Line color for this route
+//             strokeOpacity: 1.0,
+//             strokeWeight: 2, // Line width
+//         });
+
+//         routePath.setMap(map);
+//     }
+
+//     // Display multiple routes
+//     displayRoute(route1Data, "#FF0000"); // Red color for route 1
+//     displayRoute(route2Data, "#00FF00"); // Green color for route 2
+//     // Add more routes if needed...
+// }
+
